228fca309fd79707f0c4d4ebca63e43ea6b57a59
josef kaiser <david.raese@protonmail.com>
2020-09-28 17:36:11 +0200

    added Contact class and basic test in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Testing Contact Class
+//        Contact newContact = new Contact("david raese", "+43 677 627489");
+//        String contactStringTest = newContact.toString();
+//        System.out.println(contactStringTest);
     }
 }
